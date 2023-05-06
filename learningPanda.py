c1fe2413b2ccedf9c6b639798ad421055d433a77
ayebaishmo <ishmo255super@gmail.com>
2023-05-06 12:09:54 +0300

    Innitial commit for panda

---
+++ learningPanda.py
@@ -0,0 +1,8 @@
+import panda as pd # This is always assumed but is included here as an introduction.
+import numpy as np
+import matplotlib as plt
+np.random.seed(0)
+values = np.random.randn(100) # array of normally distributed random numbers
+s = pd.Series(values) # generate a pandas series
+s.plot(kind='hist', title='Normally distributed random values') # hist computes distribution
+plt.show()
