00cf935ff2159fe2aec6ef59f85b5fd1c828a214
ayebaishmo <ishmo255super@gmail.com>
2023-05-06 10:40:03 +0300

    Accessing content of age column using a file name

--- test.py
+++ test.py
@@ -1,33 +1,43 @@
 import numpy as np
-a = np.array([1,2,3])
-print(a)
+"""a = np.array([1,2,3])
+print(a)"""
 
 # More than one dimension
-b = np.array([[1,2],[4,5]])
-print(b)
+"""b = np.array([[1,2],[4,5]])
+print(b)"""
 
 # Minimum dimension
 
-c = np.array([1,2,3,4,5], ndmin = 2)
-print(c)
+"""c = np.array([1,2,3,4,5], ndmin = 2)
+print(c)"""
 
 #dtype paramenter
-
+"""
 d = np.array([1,2,3], dtype=complex)
-print(d)
+print(d)"""
 
 # DATA TYPE OBJECTS (dtype)
-dt = np.dtype(np.int32)
-print(dt)
+"""dt = np.dtype(np.int32)
+print(dt)"""
 
 # int8, int16, int32, int64 can be replaced by equivalent string 'i1', 'i2','i4', etc
-dt = np.dtype('i4')
-print(dt)
+"""dt = np.dtype('i4')
+print(dt)"""
 
 # Using edian notation
-dt = np.dtype('>i4')
-print(dt)
+"""dt = np.dtype('>i4')
+print(dt)"""
 
 # Structured data type
+"""# dt = np.dtype([('age', np.int8)])
+print(dt)"""
+
+# Applying it in ndarray object
+"""dt = np.dtype([('age', np.int8)])
+q = np.array([(10,),(20,),(30)], dtype=dt)
+print(q)"""
+
+# file name can be used to access content of age column
 dt = np.dtype([('age', np.int8)])
-print(dt)
+a = np.array([(10,),(20,),(30,)], dtype=dt)
+print(a['age'])
